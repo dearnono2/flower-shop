3b42f1de0a19004082b5999587571dba61ece669
dearnono2 <dearnono2@gmail.com>
2022-11-28 17:47:41 +0900

    메인페이지 banner 영역 스크롤 이벤트 추가

--- js/main.js
+++ js/main.js
@@ -15,3 +15,32 @@ btnCall.onclick = function(e) {
   // menuMo에 on이 있으면 제거하고, 없으면 추가
   menuMo.classList.toggle("on");
 }
+
+
+/* section - banner 영역 */
+const banner = document.querySelector('#banner');
+const wrapitems = banner.querySelector('.wrap');
+const item = banner.querySelectorAll('.item');
+
+
+
+window.addEventListener('scroll', (e) => {
+  console.log(window.scrollY);
+  if(window.scrollY > 1750) {
+    item[0].classList.add('show');
+  } else if (window.scrollY > 4000) {
+    item[0].classList.remove('show');
+  }
+
+  if(window.scrollY > 2150) {
+    item[1].classList.add('show');
+  } else if (window.scrollY > 4800) {
+    item[1].classList.remove('show');
+  }
+
+  if(window.scrollY > 2700) {
+    item[2].classList.add('show');
+  } else if (window.scrollY > 5600) {
+    item[2].classList.remove('show');
+  }
+})
