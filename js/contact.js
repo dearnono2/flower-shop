b554bcf19fd52cd7ae76448f793ad3b3fcd3157b
dearnono2 <dearnono2@gmail.com>
2022-11-28 15:41:15 +0900

    지도 오류 수정

--- js/contact.js
+++ js/contact.js
@@ -11,21 +11,22 @@ let zoom = true;
 
 
 var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(37.507025, 126.7563481), //지도의 중심좌표.
+	center: new kakao.maps.LatLng(37.497952, 127.027619), //지도의 중심좌표.
 	level: 3 //지도의 레벨(확대, 축소 정도)
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-var markerOptions = [
-  {
-    title: '본점',
-    latlng: new kakao.maps.LatLng(37.507025, 126.7563481),
-    imgSrc: 'img/marker1.png',
-    imgSize: imageSize = new kakao.maps.Size(232, 99),
-    imgPos: {offset: new kakao.maps.Point(116, 69)},
-  }
-]
+// 마커가 표시될 위치 
+var markerPosition  = new kakao.maps.LatLng(37.497952, 127.027619); 
+
+// 마커를 생성
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커 지도위에 표시
+marker.setMap(map);
 
 
 function moveTo(target) {
@@ -34,11 +35,11 @@ function moveTo(target) {
 }
 
 
-// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성
 var mapTypeControl = new kakao.maps.MapTypeControl();
 map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
-// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성
 var zoomControl = new kakao.maps.ZoomControl();
 
 
